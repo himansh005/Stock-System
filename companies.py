0949f6ef1cacd08a8321ecfeea22377ab15c642a
Himanshu <35147981+himansh005@users.noreply.github.com>
2018-12-25 19:43:55 +0530

    Delete companies.py

--- companies.py
+++
@@ -1,3 +0,0 @@
-def exporter(self,stock):
-        output = "{} {} {} {},{} {}".format(str(stock.sid),stock.type ,stock.name,str(stock.initqty),str(stock.qty),"CLOSED")
-        TransactionRoom.printOutput[int(stock.sid)] = output
